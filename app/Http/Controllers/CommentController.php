6dedd80bfecb8fce1ccad5ddb7fccbe5c7519efc
IndianGhost <achraf.bellaali@gmail.com>
2022-09-02 16:17:30 +0100

    upgrading To 6.0 From 5.8

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\Input;
 use App\Comment;
 use App\Post;
 
@@ -13,7 +12,7 @@ class CommentController extends HomeController
     public function store(Request $request)
     {
         $validator = Validator::make($request->all(), Comment::$rules);
-        $post_id = Post::find(Input::get('post_id'));
+        $post_id = Post::find(Request::input('post_id'));
         if($validator->fails())
         {
             return redirect(route('post', $post_id))
